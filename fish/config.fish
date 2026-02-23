b9626241e989791ccf4aae67a62cf1945791aecf
Jake <jrunge@precisionplanting.com>
2026-02-23 15:41:16 -0600

    fish aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
+    set -U fish_greeting ""
+    zoxide init fish | source
 end
